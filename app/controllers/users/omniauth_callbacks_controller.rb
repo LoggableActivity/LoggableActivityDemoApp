d50accc3a6fb6f42a6e51f3d30f3d202b2272db9
Max Groenlund <max@synthmax.dk>
2024-07-19 08:27:14 +0200

    Fix: Rename User to User in sessions

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-module User
+module Users
   class OmniauthCallbacksController < Devise::OmniauthCallbacksController
     # You should configure your model like this:
     # devise :omniauthable, omniauth_providers: [:twitter]
