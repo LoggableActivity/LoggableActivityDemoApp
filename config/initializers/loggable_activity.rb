e39ec65cb277ab7b9855afe55cfca8b2be5d7de8
Max Groenlund <max@synthmax.dk>
2024-07-21 16:23:23 +0200

    Updated actor name from email to full_name

--- config/initializers/loggable_activity.rb
+++ config/initializers/loggable_activity.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
 LoggableActivity.actor_model_name = 'User'
-LoggableActivity.fetch_actor_name_from = 'email'
+LoggableActivity.fetch_actor_name_from = 'full_name'
 LoggableActivity.config_file_path = Rails.root.join('config/loggable_activity.yaml')
 LoggableActivity.task_for_sanitization = false
